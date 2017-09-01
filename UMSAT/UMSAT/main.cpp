acb2ce5330844ce801513c619c6a27ef84113992
WXIONG11 <wxiong11@ford.com>
2017-08-31 20:05:16 -0700

    compatible with xcode (ceres_1.13.0)

---
+++ UMSAT/UMSAT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UMSAT
+//
+//  Created by Xiong, Weifeng (W.) on 8/29/17.
+//  Copyright © 2017 Xiong, Weifeng (W.). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
